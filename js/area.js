f6b18f6723f4318e1c2f4c6a6befffbf563df892
“Rajul” <rajulkoshta10@gmail.com>
2021-09-13 01:51:44 +0530

    changed and updates files

---
+++ js/area.js
@@ -0,0 +1,37 @@
+const baSe = document.querySelector("#base");
+const heiGht = document.querySelector("#height");
+const calculateBtn = document.querySelector(".btn-primary");
+const reloadBtn = document.querySelector(".btn-secondary");
+const message = document.querySelector("#message");
+
+
+calculateBtn.addEventListener("click",calculateAreaOfTriangle);
+
+function calculateAreaOfTriangle(){
+    const b = baSe.value;
+    const h = heiGht.value;
+    const area = calculateArea(b,h);
+    if(area === ""){
+        showMessage("invalid input");
+    }else{
+        showMessage("Area of triangle is : " + area);
+    }
+}
+function calculateArea(b,h){
+    let area = (1/2)* b * h;
+    return area;
+}
+
+
+function hideMessage(){
+    message.style.display = "none";
+}
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg ;
+}
+
+reloadBtn.addEventListener("click",refresh);
+function refresh(){
+    window.location.reload("refresh");
+}
---
+++ js/hypotenuse.js
@@ -0,0 +1,37 @@
+const sideOne = document.querySelector("#side1");
+const sideTwo = document.querySelector("#side2");
+const calculateBtn = document.querySelector(".btn-primary");
+const reloadBtn = document.querySelector(".btn-secondary");
+const message = document.querySelector("#message");
+
+
+calculateBtn.addEventListener("click",calculateHypotenuse);
+
+function calculateHypotenuse(){
+    const a = sideOne.value;
+    const b = sideTwo.value;
+    const LenOfHypo = calculateLengthOfHypotenuse(a,b);
+    if(LenOfHypo === ""){
+        showMessage("invalid sides");
+    }else{
+        showMessage("length of hypotenuse is : " + LenOfHypo);
+    }
+}
+function calculateLengthOfHypotenuse(a,b){
+    let len = Math.sqrt((a * a) + (b * b));
+    return len;
+}
+
+
+function hideMessage(){
+    message.style.display = "none";
+}
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg ;
+}
+
+reloadBtn.addEventListener("click",refresh);
+function refresh(){
+    window.location.reload("refresh");
+}
